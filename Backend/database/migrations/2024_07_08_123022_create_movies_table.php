89bd81152d18ddaa126ce91eb0ca465990035771
dohaseif2 <dohaseif2@gmail.com>
2024-07-10 16:22:14 +0300

    feat(database): create migration files

---
+++ Backend/database/migrations/2024_07_08_123022_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('image');
+            $table->text('description');
+            $table->text('video');
+            $table->dateTime('release_date');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('language_id');
+            $table->timestamps();
+            $table->softDeletes(); 
+    
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('language_id')->references('id')->on('languages')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
